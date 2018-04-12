37daf93a3efd124e8b40425edd517c2017ac7f5f
Caden MacKenzie <cadenmackenzie@gmail.com>
2018-04-12 16:47:37 +0800

    first round review comments

--- darkSky_security/src/main/java/com/what/ever/Application.java
+++ darkSky_security/src/main/java/com/what/ever/Application.java
@@ -8,8 +8,36 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+/*
+Caden:
+A few general project comments:
+
+It would be good to separate each project in the folder into their own intellij
+projects and their own Github repos. This is generally a good thing to do for organization. If you want
+to try to add a feature or something, make a new branch, make those changes and then you can merge afterwards
+or just keep them on separate branches both of which can be on Github.
+
+The project looks awesome. From here it would be good to go through and clean up any
+code you have commented out and no longer need or any files you aren't using. It looks like
+you are using api key auth instead of the bCryptPasswordEncoder so that would be something you
+can eliminate. Just general cleaning up.
+
+I added a new package in the resources package for DB dumps. Its nice to have for anyone
+that wants to use the the program. The dumps you gave me have some data which is nice but you don't
+want to include that you can just dump the db structure as well.
+
+It looks like you took out the code that added the lat/long api info in your database. It looks like you
+were on your way so id like too see that integrated because then your application would be using a db
+for authentication and for storing data from an api call. You could then do interesting things with the
+data or integrate more than one third party api.
+
+Get started on those and then Ill take another look. Nice job!
+
+
+ */
+
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application /*extends SpringBootServletInitializer*/ {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -20,8 +48,8 @@ public class Application extends SpringBootServletInitializer {
         return builder.build();
     }
 
-    @Override
-    protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
-        return application.sources(Application.class);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
+//        return application.sources(Application.class);
+//    }
 }
---
+++ darkSky_security/src/main/resources/db/customkey.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: 127.0.0.1    Database: custom_key
+-- ------------------------------------------------------
+-- Server version	5.7.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(100) NOT NULL,
+  `lastName` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `isActive` tinyint(4) NOT NULL,
+  `apiKey` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`,`firstName`,`lastName`,`email`,`isActive`,`apiKey`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Brandt','Leeds','brandt@gmail.com',0,'1b5e5493-e3f7-4b06-8ba1-a2bc83f4acb9'),(2,'Ben','Jameson','benjam@gmail.com',0,'accf525c-54ac-4ab6-87ca-a39eb41f79d0'),(3,'Tony','Money','tonmon@gmail.com',0,'685890ee-47b3-41e0-a927-a31ad52f4dfa'),(4,'John','Hoopy','jhoop@gmail.com',0,'65ad8a7e-5160-4190-a47f-d1850049dbe9');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-12 15:11:53
